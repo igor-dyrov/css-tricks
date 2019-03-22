f708c08048df7e7aaf8a6661ad171d1e3cfbc788
igor-dyrov <bazbazbaz.id@gmail.com>
2019-03-22 23:28:44 +0300

    develop

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 app.listen(4000);
