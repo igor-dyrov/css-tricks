51b6871eeb58e38ff8251e2b7636488fab0479b9
igor-dyrov <bazbazbaz.id@gmail.com>
2019-03-23 00:43:15 +0300

    simple react project

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static('../src'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/src/index.html'));
