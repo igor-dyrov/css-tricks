d391ff0b353e36123db721a3566e8760bcb223de
igor-dyrov <bazbazbaz.id@gmail.com>
2019-03-29 22:05:32 +0300

    sing out is left

---
+++ server/server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const errorHandler = require('errorhandler');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(errorHandler());
+
+const MongoClient = require('mongodb').MongoClient;
+
+const url = "mongodb://igor:261097@localhost/";
+
+const RESPONSE_CODES = {
+	OK: 200,
+	FORBIDDEN: 403,
+	NOT_AUTHORIZED: 401,
+	CONFLICT: 409
+};
+
+app.use('/about', (req, res) => {
+	res.send('About this wiki');
+});
+
+app.post('/api/register', (req, res) => {
+	if (!req.body || !req.body.login || !req.body.password) {
+		res.status(RESPONSE_CODES.FORBIDDEN);
+		res.json({
+			message: 'Wrong request body format'
+		});
+	} else {
+		const md5sum = crypto.createHash('md5');
+		console.log(req.body.login + req.body.password);
+		md5sum.update(req.body.login + req.body.password);
+		MongoClient.connect(url, (err, db) => {
+			if (err) throw err;
+			const dbo = db.db("authDB");
+			dbo.collection("sessions").findOne({login: req.body.login}, (err, result) => {
+				if (!err && result) {
+					res.status(RESPONSE_CODES.CONFLICT);
+					res.json({
+						message: 'User already exists'
+					});
+				} else {
+					const toInsert = {};
+					toInsert.login = req.body.login;
+					toInsert.password = req.body.password;
+					toInsert.sessionID = md5sum.digest('hex');
+					dbo.collection("sessions").insertOne(toInsert, (err, result) => {
+						if (!err) {
+							res.set('Set-Cookie', toInsert.sessionID);
+							res.status(RESPONSE_CODES.OK);
+							res.json(toInsert);
+						}
+					})
+				}
+				db.close();
+			});
+		});
+	}
+});
+
+app.post('/api/session', (req, res) => { //login
+	if (!req.body || !req.body.login || !req.body.password) {
+		res.status(RESPONSE_CODES.FORBIDDEN);
+		res.json({
+			message: 'Wrong request body format'
+		});
+	} else {
+		MongoClient.connect(url, (err, db) => {
+			const dbo = db.db("authDB");
+			dbo.collection("sessions").findOne({login: req.body.login, password: req.body.password}, (err, result) => {
+				if (!err && result) {
+					res.status(RESPONSE_CODES.OK);
+					res.set('Set-Cookie', result.sessionID);
+					res.json(result);
+				} else {
+					res.status(RESPONSE_CODES.FORBIDDEN);
+					res.json({
+						message: 'Wrong login or password'
+					});
+				}
+			});
+		});
+	}
+});
+
+app.use('/api/session', (req, res) => { //check auth
+	const session = req.get('Cookie');
+	MongoClient.connect(url, (err, db) => {
+		if (err) throw err;
+		const dbo = db.db("authDB");
+		dbo.collection("sessions").findOne({sessionID: session}, (err, result) => {
+			if (!err && result) {
+				res.status(RESPONSE_CODES.OK);
+				console.log(result);
+				res.json(result);
+			} else {
+				res.status(RESPONSE_CODES.NOT_AUTHORIZED);
+				res.json({
+					message: 'not authorized'
+				});
+			}
+		});
+	});
+});
+
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
