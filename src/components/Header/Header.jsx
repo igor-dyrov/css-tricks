9fb6ef9af4e52b99277f516049f077aaabc10a05
igor-dyrov <bazbazbaz.id@gmail.com>
2019-04-16 19:40:05 +0300

    everyhing is done except mobile design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import history from '../../middleware/history/history.js';
 import Button from '../Button/Button.jsx';
+import NavigationButton from '../NavigationButton/NavigationButton.jsx';
 import { PATHS } from '../../routes.jsx';
 import setAuthData from '../../redux/auth/auth.action.js';
 import UserService from '../../services/UserService/UserService.js';
@@ -17,22 +18,22 @@ class Header extends React.Component {
 			history.push(PATHS.SIGN_IN);
 		}
 	}
-	
+
 	static signUpOnClick() {
 		if (history.location.pathname !== PATHS.SIGN_UP) {
 			history.push(PATHS.SIGN_UP);
 		}
 	}
-	
+
 	static logoOnClick() {
 		if (history.location.pathname !== PATHS.MENU) {
 			history.push(PATHS.MENU);
 		}
 	}
-	
+
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
 			mobileMenuIsVisible: false,
 		};
@@ -57,7 +58,7 @@ class Header extends React.Component {
 
 	signOutOnClick = () => {
 		const { setAuthInfo } = this.props;
-		
+
 		UserService.logOut()
 			.then(() => {
 				setAuthInfo({
@@ -66,11 +67,34 @@ class Header extends React.Component {
 				});
 			});
 	};
-	
+
 	render() {
 		const { isAuthorized } = this.props;
 		const { userName } = this.props;
 		const { mobileMenuIsVisible } = this.state;
+
+		const mobileNavigation = isAuthorized ? [
+			<NavigationButton
+				text={userName}
+				imageSource='./static/img/user.png'
+			/>,
+			<NavigationButton
+				text='Log Out'
+				imageSource='./static/img/exit.png'
+				onClick={this.signOutOnClick}
+			/>
+		] : [
+			<NavigationButton
+				text='Sign In'
+				imageSource='./static/img/signIn.png'
+				onClick={Header.signInOnClick}
+			/>,
+			<NavigationButton
+				text='Sign Up'
+				imageSource='./static/img/signUp.png'
+				onClick={Header.signUpOnClick}
+			/>
+		];
 
 		return (
 			<header>
@@ -82,44 +106,17 @@ class Header extends React.Component {
 					<Button className='header-search__button' text='Find'/>
 					<input className='header-search__input'/>
 				</div>
-				{!isAuthorized ? (
-					<nav>
-						<div className='navigation-button' onClick={Header.signInOnClick}>
-							<img src='./static/img/signIn.png' className='navigation-button__image'/>
-							<div className='navigation-button__text'>Sign In</div>
-						</div>
-						<div className='navigation-button' onClick={Header.signUpOnClick}>
-							<img src='./static/img/signUp.png' className='navigation-button__image'/>
-							<div className='navigation-button__text'>Sign Up</div>
-						</div>
-					</nav>
-				) : (
-					<nav>
-						<div className='navigation-button'>
-							<img src='./static/img/user.png' className='navigation-button__image'/>
-							<div className='navigation-button__text'>{userName}</div>
-						</div>
-						<div className='navigation-button'>
-							<img src='./static/img/exit.png' className='navigation-button__image'/>
-							<div className='navigation-button__text' onClick={this.signOutOnClick}>Log Out</div>
-						</div>
-					</nav>
-				)}
+				<nav>
+					{mobileNavigation}
+				</nav>
 				<div className='header__mobile-button' onClick={this.mobileMenuOnClick}>
 					<div className='line'/>
 					<div className='line'/>
 					<div className='line'/>
 				</div>
-				{mobileMenuIsVisible ? (
+				{ mobileMenuIsVisible ? (
 					<div className='mobile-navigation'>
-						<div className='navigation-button' onClick={Header.signInOnClick}>
-							<img src='./static/img/signIn.png' className='navigation-button__image'/>
-							<div className='navigation-button__text'>Sign In</div>
-						</div>
-						<div className='navigation-button' onClick={Header.signUpOnClick}>
-							<img src='./static/img/signUp.png' className='navigation-button__image'/>
-							<div className='navigation-button__text'>Sign Up</div>
-						</div>
+						{mobileNavigation}
 					</div>
 				) : ''}
 			</header>
