4f97ed559d6cefa712d144a9b8ac55fe5d1712af
igor-dyrov <bazbazbaz.id@gmail.com>
2019-04-25 19:12:20 +0300

    almost done banner

--- src/containers/BestService/BestService.tsx
+++ src/containers/BestService/BestService.tsx
@@ -14,12 +14,32 @@ const BestService = () => (
 				<div className='headerBestService__menu'>
 					<div className='headerBestService__link'>Home</div>
 					<div className='headerBestService__link'>About</div>
-					<div className='headerBestService__link'>Services</div>
+					<div className='headerBestService__link headerBestService__link_active'>Services</div>
 					<div className='headerBestService__link'>Portfolio</div>
 					<div className='headerBestService__link'>Shop</div>
 					<div className='headerBestService__link'>Contacts</div>
 					<div className='headerBestService__link'>Pages</div>
 				</div>
+				<div className='headerBestService__contact'>
+					<img src='./static/img/BestService/fb.png' className='headerBestService__social-link'/>
+					<img src='./static/img/BestService/tw.png' className='headerBestService__social-link'/>
+					<img src='./static/img/BestService/g+.png' className='headerBestService__social-link'/>
+				</div>
+			</div>
+			<div className='bannerBestService__nav bannerBestService__nav_position_left'>
+				<div className='bannerBestService__nav-button bannerBestService__nav-button_position_left'/>
+			</div>
+			<div className='bannerBestService__nav bannerBestService__nav_position_right'>
+				<div className='bannerBestService__nav-button bannerBestService__nav-button_position_right'/>
+			</div>
+			<div className='bannerBestService__description'>
+				<h1 className='bannerBestService__service-text'>The Best Services</h1>
+				<h2 className='bannerBestService__description-text'>
+					We provide the best services in the world
+					We provide the best services in the world
+					We provide the best services in the world
+				</h2>
+				<div className='bannerBestService__button'>Hire us</div>
 			</div>
 		</div>
 	</React.Fragment>
